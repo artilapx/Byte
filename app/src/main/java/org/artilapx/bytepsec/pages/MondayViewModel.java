b604c108d9f0f3f551f805bb8b04636d3e5dc390
artilapx <artyom.lapscin@yandex.ru>
2020-09-28 14:53:01 +0300

    Update launcher icon

--- app/src/main/java/org/artilapx/bytepsec/pages/MondayViewModel.java
+++ app/src/main/java/org/artilapx/bytepsec/pages/MondayViewModel.java
@@ -1,11 +1,14 @@
 package org.artilapx.bytepsec.pages;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import org.artilapx.bytepsec.App;
+import org.artilapx.bytepsec.R;
 import org.artilapx.bytepsec.models.Schedule;
 import org.artilapx.bytepsec.models.ScheduleResponse;
 import org.artilapx.bytepsec.source.WebHandler;
@@ -46,9 +49,13 @@ public class MondayViewModel extends ViewModel {
             public void onResponse(Call<ScheduleResponse> call, Response<ScheduleResponse> response) {
                 if (response.isSuccessful()) {
                     Log.i(TAG, "Success!");
+                    Toast.makeText(App.getInstance().getApplicationContext(),
+                            "ok", Toast.LENGTH_SHORT).show();
                     setScheduleList(response.body().getScheduleList());
                 } else {
                     Log.i(TAG, "Got error code: " + response.code());
+                    Toast.makeText(App.getInstance().getApplicationContext(),
+                            R.string.error_bad_response, Toast.LENGTH_SHORT).show();
                     setScheduleList(new ArrayList<>());
                 }
             }
